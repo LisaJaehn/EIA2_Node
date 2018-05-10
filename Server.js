08df51e3dde065d2d197c0b779bc1dd27821348b
lisaj <lisaj@192.168.0.228>
2018-05-10 13:51:09 +0200

    Server hochladen

--- Server.js
+++ Server.js
@@ -1,7 +1,8 @@
-// HTTP Objekt wird im Code erstellt 
-// Interpreter sucht nach jedem möglichen Import im http- Modul  und wird ihn einzeln an das http- Objekt im Code anhängen
+//Bindet Url Modul mit ein
 "use strict";
 const Url = require("url");
+//HTTP Objekt wird im Code erstellt 
+//Interpreter sucht nach jedem möglichen Import im http- Modul  und wird ihn einzeln an das http- Objekt im Code anhängen
 const Http = require("http");
 //namespace erstellen
 var Node;
@@ -10,43 +11,51 @@ var Node;
     //Variable port wird erstellt, als number
     //process.env.PORT= Port wird als Umgebungsvariable festgelegt, um dem Webserver mitzuteilen, welcher Port überwacht werden soll.
     let port = process.env.PORT;
-    //Wenn der Port nicht definiert ist, liegt er bei 8100
+    //Wenn der Port nicht definiert ist, liegt er bei Nummer 8100
     //8100 (Portnummer) ist der Port, dem der Server zuhören soll
     if (port == undefined)
         port = 8100;
-    //Ereugung des Serverobjektes, Variable server, um immer wieder auf den Server zugreigen zu können
+    //Ereugung des Serverobjektes, Variable server, um mit dem Server weiter arbeiten zu können (Zugriff möglich)
     let server = Http.createServer();
     //Wenn der Server zuhört, wird die Funktion handleListen ausgeführt
     server.addListener("listening", handleListen);
+    // Server beibringen auf etwas zu hören
     server.addListener("request", handleRequest);
     server.listen(port);
     //Funktion handleListen wird erstellt, wenn der Server zuhört wird in der Konsole "Ich höre" ausgegeben
     //In der Funktion werden keine Parameter benötigt
     function handleListen() {
+        //Ausgabe im Terminal bzw. Konsole
         console.log("Ich höre?");
     }
-    //Funktion handleRequest wird erstellt, 2 Parameter werden festgelegt
+    //Funktion handleRequest wird erstellt, 2 Parameter werden festgelegt, ohne Rückgabewert
     //Die einkommenden Information werden bearbeitet und wieder zurück geschickt
     //_request: Http.IncomingMessage: Einkommende Informationen
     //_response: Http.ServerResponse: Bearbeitete Informationen
     function handleRequest(_request, _response) {
+        //Ausgabe im Terminal bzw. Konsole
         console.log("Ich höre Stimmen!");
         //Variable query wird erstellt
         //Übersetzung in ein assoziatives Array
+        // umwandeln von /?a=10&b=20 in ein JavaScript-Objekt
         let query = Url.parse(_request.url, true).query;
+        //Definiere a als Variable
         let a = parseInt(query["a"]);
+        //Definiere a als Variable
         let b = parseInt(query["b"]);
-        //Umlautprobleme werden behoben
+        //Umlautprobleme werden behoben, Schriftart wird dadurch ebenfalls geändert
         _response.setHeader("content-type", "text/html; charset=utf-8");
+        //Wird für alle zugänglich gemacht
         _response.setHeader("Access-Control-Allow-Origin", "*");
-        //Wenn der Server zugehört und die Daten bearbeitet wurden wird das Ergebnis ausgegben
+        //Ausgabe im Terminal bzw. Konsole
         _response.write("Ich habe dich gehört<br/>");
         //Schlüssel wird durchgegeben
         for (let key in query)
             //console.log(query[key]);
             //Ausgabe der Queryinformation (a, b)
             _response.write("Query-Informationen, die eingegeben wurden: " + (query[key]) + "<br>");
-        //Ausgabe Ergebnis
+        //Wenn der Server zugehört und die Daten bearbeitet wurden wird das Ergebnis ausgegben
+        //Lösung erscheint im Browserfenster
         _response.write("Das Ergebnis ist: " + (a + b));
         //Ende, Inforamtion wird zum Nutzer geschickt
         _response.end();
--- Server.ts
+++ Server.ts
@@ -1,7 +1,10 @@
-// HTTP Objekt wird im Code erstellt 
-// Interpreter sucht nach jedem möglichen Import im http- Modul  und wird ihn einzeln an das http- Objekt im Code anhängen
+//Bindet Url Modul mit ein
 
 import * as Url from "url";
+
+//HTTP Objekt wird im Code erstellt 
+//Interpreter sucht nach jedem möglichen Import im http- Modul  und wird ihn einzeln an das http- Objekt im Code anhängen
+
 import * as Http from "http";
 
 //namespace erstellen
@@ -13,6 +16,7 @@ namespace Node {
     interface AssocStringString {
 
         //String als Schlüssel, der Wert ist ebenfalls ein String
+        //Homogenes assoziatives Array
 
         [key: string]: string;
     }
@@ -23,19 +27,22 @@ namespace Node {
 
     let port: number = process.env.PORT;
 
-    //Wenn der Port nicht definiert ist, liegt er bei 8100
+    //Wenn der Port nicht definiert ist, liegt er bei Nummer 8100
     //8100 (Portnummer) ist der Port, dem der Server zuhören soll
 
     if ( port == undefined )
         port = 8100;
 
-    //Ereugung des Serverobjektes, Variable server, um immer wieder auf den Server zugreigen zu können
+    //Ereugung des Serverobjektes, Variable server, um mit dem Server weiter arbeiten zu können (Zugriff möglich)
 
     let server: Http.Server = Http.createServer();
 
     //Wenn der Server zuhört, wird die Funktion handleListen ausgeführt
 
     server.addListener( "listening", handleListen );
+
+    // Server beibringen auf etwas zu hören
+
     server.addListener( "request", handleRequest );
     server.listen( port );
 
@@ -43,30 +50,46 @@ namespace Node {
     //In der Funktion werden keine Parameter benötigt
 
     function handleListen(): void {
+
+        //Ausgabe im Terminal bzw. Konsole
+
         console.log( "Ich höre?" );
     }
 
-    //Funktion handleRequest wird erstellt, 2 Parameter werden festgelegt
+    //Funktion handleRequest wird erstellt, 2 Parameter werden festgelegt, ohne Rückgabewert
     //Die einkommenden Information werden bearbeitet und wieder zurück geschickt
     //_request: Http.IncomingMessage: Einkommende Informationen
     //_response: Http.ServerResponse: Bearbeitete Informationen
 
     function handleRequest( _request: Http.IncomingMessage, _response: Http.ServerResponse ): void {
+
+        //Ausgabe im Terminal bzw. Konsole
+
         console.log( "Ich höre Stimmen!" );
 
         //Variable query wird erstellt
         //Übersetzung in ein assoziatives Array
+        // umwandeln von /?a=10&b=20 in ein JavaScript-Objekt
 
         let query: AssocStringString = Url.parse( _request.url, true ).query;
+
+        //Definiere a als Variable
+
         let a: number = parseInt( query["a"] );
+
+        //Definiere a als Variable
+
         let b: number = parseInt( query["b"] );
 
-        //Umlautprobleme werden behoben
+        //Umlautprobleme werden behoben, Schriftart wird dadurch ebenfalls geändert
 
         _response.setHeader( "content-type", "text/html; charset=utf-8" );
+
+        //Wird für alle zugänglich gemacht
+
         _response.setHeader( "Access-Control-Allow-Origin", "*" );
 
-        //Wenn der Server zugehört und die Daten bearbeitet wurden wird das Ergebnis ausgegben
+        //Ausgabe im Terminal bzw. Konsole
 
         _response.write( "Ich habe dich gehört<br/>" );
 
@@ -79,7 +102,8 @@ namespace Node {
 
             _response.write( "Query-Informationen, die eingegeben wurden: " + ( query[key] ) + "<br>" );
 
-        //Ausgabe Ergebnis
+        //Wenn der Server zugehört und die Daten bearbeitet wurden wird das Ergebnis ausgegben
+        //Lösung erscheint im Browserfenster
 
         _response.write( "Das Ergebnis ist: " + ( a + b ) );
 
